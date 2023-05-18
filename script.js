697d60c9cf3349a58d93ac73020c0ca2ba7431c6
rahul4328 <125386949+rahul4328@users.noreply.github.com>
2023-05-18 09:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	  return key in sampleObject;
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+console.log(hasKey("red"));   // true
+console.log(hasKey("blue"));  // false
+console.log(hasKey("white")); // true
